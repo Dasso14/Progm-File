3af11d46f156d25e6cae1326bccd874e373495b0
Julio Centeno <129196731+Dasso14@users.noreply.github.com>
2023-10-24 10:44:07 -0500

    Add files via upload

---
+++ Interfaz grafica/App.java
@@ -0,0 +1,12 @@
+// Prueba de LabelFrame
+import javax.swing.JFrame;
+public class App
+{
+    public static void main(String[] args)
+    {
+        LabelFrame marcoEtiqueta = new LabelFrame();
+        marcoEtiqueta.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoEtiqueta.setSize(260, 180);
+        marcoEtiqueta.setVisible(true);
+    }
+}// Fin de App Prueba Label
